42a2d00b7b85cc0befd258a18749851a42fa591d
Andrei <andreichebkuvalda@gmail.com>
2023-06-30 13:20:53 +0300

    feat:add catalog menu and handler 30.06.2023 13:20

---
+++ chebProSport/src/components/catalog/catalogMenu/button.tsx
@@ -0,0 +1,79 @@
+import React, { useState, MouseEvent } from 'react';
+import { ListItem, ListItemButton, ListItemText } from '@mui/material';
+
+type ButtonMenuConfig = {
+  name: string;
+  content?: Array<string>;
+};
+
+const ButtonCatalogMenu = (prop: ButtonMenuConfig) => {
+  const { name } = prop;
+  const { content } = prop;
+
+  const [openButton, setOpenButton] = useState<string>('none');
+
+  const handleClickButton = (event: MouseEvent) => {
+    const svg = event.currentTarget.querySelector('.svg-button-catalog');
+
+    if (openButton === 'block') {
+      svg ? svg.classList.remove('svg-button-catalog-show') : null;
+      setOpenButton('none');
+    } else {
+      setOpenButton('block');
+      svg ? svg.classList.add('svg-button-catalog-show') : null;
+    }
+  };
+
+  return (
+    <>
+      <ListItem sx={{ padding: '0' }} onClick={handleClickButton}>
+        <ListItemButton
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <svg
+            width='10'
+            height='18'
+            viewBox='0 0 10 18'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            style={{ marginRight: '10px' }}
+            className='svg-button-catalog'
+          >
+            <path
+              d='M0.962078 17.3594V0.640625L9.32145 9L0.962078 17.3594Z'
+              fill='#772BD8'
+            />
+          </svg>
+          <ListItemText primary={name} />
+        </ListItemButton>
+      </ListItem>
+      {content && openButton === 'block' ? (
+        <>
+          {content.map((item) => {
+            return (
+              <>
+                <ListItem sx={{ padding: '0' }}>
+                  <ListItemButton
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <ListItemText sx={{ marginLeft: '28px' }} primary={item} />
+                  </ListItemButton>
+                </ListItem>
+              </>
+            );
+          })}
+        </>
+      ) : (
+        ''
+      )}
+    </>
+  );
+};
+
+export default ButtonCatalogMenu;
