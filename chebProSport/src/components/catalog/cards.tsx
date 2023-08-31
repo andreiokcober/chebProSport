5d004839003dd676d3e5a53ea017acb0fcb318a8
Andrei <andreichebkuvalda@gmail.com>
2023-08-31 10:28:22 +0300

    feat:refactor code and add MissingCards component 31.08.23 10:28

--- chebProSport/src/components/catalog/cards.tsx
+++ chebProSport/src/components/catalog/cards.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@mui/material';
 
 import Item from './Item';
+import MissingCards from './MissingCards';
 import CardCatalog from '../../data/catalog';
 
 import { useGlobalSortCard } from '.';
@@ -9,19 +10,18 @@ import { useGlobalSortCard } from '.';
 const CardsContainer = () => {
   const { sortCard } = useGlobalSortCard();
 
-  const CardFilterCatalog = CardCatalog.filter((item) => {
-    if (sortCard === '') {
-      return item;
-    } else if (item.type.toLowerCase() === sortCard.toLowerCase()) {
-      return item;
-    }
+  const cardFilterCatalog = CardCatalog.filter((item) => {
+    const findItem = sortCard
+      ? item.type.toLowerCase() === sortCard.toLowerCase()
+      : item;
+    return findItem;
   });
 
   return (
     <Grid container spacing={3} sx={{ paddingTop: '20px' }}>
-      {CardFilterCatalog.map((item, i) => (
-        <Item item={item} key={i}></Item>
-      ))}
+      {cardFilterCatalog.length !== 0 &&
+        cardFilterCatalog.map((item, i) => <Item item={item} key={i} />)}
+      {cardFilterCatalog.length === 0 && <MissingCards />}
     </Grid>
   );
 };
